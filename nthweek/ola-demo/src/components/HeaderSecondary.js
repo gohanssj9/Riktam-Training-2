8cfee4bdc9ede421f7e81f09b9eada66b2d678ac
gohanssj9 <sritejaayyagari01@gmail.com>
2018-10-11 12:18:35 +0530

    Header responsive done.

--- nthweek/ola-demo/src/components/HeaderSecondary.js
+++ nthweek/ola-demo/src/components/HeaderSecondary.js
@@ -7,9 +7,15 @@ const HeaderSecondary = () => {
     <div className = "secondary-header">
       <div className = "max-width">
       <div className = "pull-left">
+        <div className = "show-xs menu-icon">
+          <img src = "https://olacabs.com/webstatic/img/menu.svg" />
+        </div>
         <a className = "hide-xs" href = "/">
           <img className = "logo" src = {logo} />
         </a>
+        <div className = "show-xs center-logo">
+          <img src = "https://olacabs.com/webstatic/img/olalogo-mb.png" />
+        </div>
       </div>
         <div className = "pull-right">
           <div className = "secondary-links hide-xs">
