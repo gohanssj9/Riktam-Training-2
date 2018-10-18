eb1af73a68dab733d1a387c9e89512b3b0146274
gohanssj9 <sritejaayyagari01@gmail.com>
2018-10-18 18:46:40 +0530

    Events clickable done. Responsiveness remaining.

--- nthweek/rtu-demo/src/components/EventsInfoComponent.js
+++ nthweek/rtu-demo/src/components/EventsInfoComponent.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import SingleEventComponent from './SingleEventComponent';
+import '../assets/information.css';
+
 const EventsInfoComponent = ({class_name}) => {
 	const name = "events-info max-width " + class_name;
 	return (
 		<div className = {name}>
+			<div className = "row">
+				<div className = "upcoming-events">
+					<div className = "title">
+						<h3 className = "text-uppercase"> Upcoming Events </h3>
+						<SingleEventComponent 
+							title = {'Our Palo Alto tournament'} 
+							subtitle = {'Palo Alto, CA'} 
+							date = {'Nov 22 - Dec 1'}
+							colorBorder = {'cyan-border-left'}
+							colorStatus = {'cyan-border'}
+							status = {'Open'} />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
