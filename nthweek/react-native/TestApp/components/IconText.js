4c87436dcee19c2e6f14f09e22a02668c44256b9
sriteja <gohanssj9>
2018-10-27 15:10:34 +0530

    IconText updated with styling.

--- nthweek/react-native/TestApp/components/IconText.js
+++ nthweek/react-native/TestApp/components/IconText.js
@@ -6,12 +6,37 @@ export default class IconText extends Component {
 	render(){
 		const {icon, text, color = '#858585', community = 'MaterialCommunityIcons'} = this.props;
 		return (
-	    <View style = {{height: 45, padding: 10, flexDirection: 'row', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-	    	<View style = {{flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 3}}>
-	    		<Icon name = {icon} type = {community} style = {{fontSize: 24, color: color}} />
-	    	</View>
-	    	<Text style = {{flex: 4,fontSize: 14.5, paddingTop: 2, justifyContent: 'center'}}> {text} </Text>
-	    </View>
+			<TouchableNativeFeedback borderLess = {false}>
+		    <View style = {styles.container}>
+		    	<View style = {styles.iconView}>
+		    		<Icon name = {icon} type = {community} style = {{fontSize: 24, color: color}} />
+		    	</View>
+		    	<Text style = {styles.textView}> {text} </Text>
+		    </View>
+	    </TouchableNativeFeedback>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		height: 45, 
+		padding: 10, 
+		flexDirection: 'row', 
+		flex: 1, 
+		alignItems: 'center', 
+		justifyContent: 'center'
+	},
+	iconView: {
+		flex: 1, 
+		justifyContent: 'center', 
+		alignItems: 'flex-start', 
+		marginLeft: 3
+	},
+	textView: {
+		flex: 4,
+		fontSize: 14.5, 
+		paddingTop: 2, 
+		justifyContent: 'center'
+	}
+});
