38eff27fdb4d334611829300f214d0a07e543594
sriteja <sritejaayyagari01@gmail.com>
2018-12-04 12:32:28 +0530

    Basic editing of datatable done.

--- nthweek/booking-app-webix-jet/sources/views/specialoffers.js
+++ nthweek/booking-app-webix-jet/sources/views/specialoffers.js
@@ -5,17 +5,17 @@ import "styles/app.css";
 export default class SpecialOffersView extends JetView {
 	config(){
 		var ui = {
-			view: "datatable", scroll: "y", id: "specialoffersgrid",
+			view: "datatable", scroll: "y", id: "specialoffersgrid", editable: true, editaction: "dblclick",
 			columns: [
 				{id: "id", header: "#", width: 42},
-				{id: "direction", header: "Direction", fillspace: 3},
-				{id: "date", header: "Date", fillspace: 1.5},
-				{id: "price", header: "Price", fillspace: 1, format: webix.i18n.priceFormat},
-				{id: "save", header: "You save", fillspace: 1, format: webix.i18n.priceFormat},
-				{id: "status", header: "Status", fillspace: 1.75, template: function(obj) {
+				{id: "direction", header: "Direction", fillspace: 3, editor: "text"},
+				{id: "date", header: "Date", fillspace: 1.5, format: webix.i18n.longDateFormatStr, editor: "date", map: "(date)#date#"},
+				{id: "price", header: "Price", fillspace: 1, format: webix.i18n.priceFormat, editor: "text"},
+				{id: "save", header: "You save", fillspace: 1, format: webix.i18n.priceFormat, editor: "text"},
+				{id: "status", header: "Status", fillspace: 1.75, editor: "select", options: ['Open', 'Available Soon', 'Last deals'], template: function(obj) {
 					let className = "";
-					if(obj.status == "Open") className = "o1";
-					else if(obj.status == "Last deals") className = "o2";
+					if(obj.status === "Open") className = "o1";
+					else if(obj.status === "Last deals") className = "o2";
 					else className = "o3";
 					return '<div class = "status ' + className + '"><p style = "margin: 0;">&#9679;&nbsp;&nbsp;' + obj.status + "</p></div>";
 				}}
@@ -44,3 +44,5 @@ export default class SpecialOffersView extends JetView {
 		});
 	}
 }
+
+// , editor: "date", map: "(date)#date#"
