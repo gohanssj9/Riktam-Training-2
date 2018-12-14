18d8f54d6bba710d1512009923a5054ac74e0167
sriteja <sritejaayyagari01@gmail.com>
2018-12-14 18:36:46 +0530

    Custom component UI done. Need to add showing, click events.

--- nthweek/booking-app-webix-jet/sources/views/register.js
+++ nthweek/booking-app-webix-jet/sources/views/register.js
@@ -1,5 +1,7 @@
 import {JetView} from 'webix-jet';
 
+import TestCustomList from 'views/testcustomlist'
+
 export default class RegisterView extends JetView {
 	config(){
 		var ui = {type: "form", rows: [
@@ -9,7 +11,12 @@ export default class RegisterView extends JetView {
 			{view: "text", label: "Login", placeholder: "Matt", labelAlign: "right", labelWidth: 100},
 			{view: "text", type: "password", label: "Password", placeholder: "********", labelAlign: "right", labelWidth: 100},
 			{view: "text", type: "password", label: "Confirm Password", placeholder: "********", labelAlign: "right", labelWidth: 100},
-			{view: "button", type: "form", value: "Register"}
+			{view: "button", type: "form", value: "Register"},
+			{view: "CustomMultiselect", label: "Random", labelAlign: "center",
+				options: [
+					{id: 1, check:1 , status: "Munching away"}
+				], value: "Add Up"
+			}
 		]};
 		return ui;
 	}
--- nthweek/booking-app-webix-jet/sources/views/testcustomlist.js
+++ nthweek/booking-app-webix-jet/sources/views/testcustomlist.js
@@ -1,55 +1,38 @@
-import {JetView} from "webix-jet";
-
-export default class TestCustomList extends JetView {
-	config() {
-		webix.protoUI({
-			name: "CustomMultiselect",
-			$init:function(config) {
-				var show_id = config.id + "_custom_select";
-				var statPopup = {
-					view: "popup", css: "custom_popup", id: show_id,
-					body: {
-						rows: [
-							{view: "datatable", header: false, scroll: false, autoheight: true, autowidth: true,
-							columns: [
-								{id: "check", template: "{common.checkbox()}", width: 25, css: "checkbox_section"},
-								{id: "status", width: 225, css: "status_section"}
-							],
-							data: config.options
-						},
-						{view: "button", type: "form", value: config.value}
-					]}							
-				};
-				this.custom_multiselect = webix.ui(statPopup);
-				this.$ready.push(this.showLabel);
-			},
-			defaults: {
-				rows: []
-			},
-			showLabel: function() {
-				var me = this;
-				var config = me.config;
-				console.log(config);
-				console.log("Inside label");
-				me.addView({
-					css: "custom_css", cols: [
-						{view: "label", label: config.label, align: config.labelAlign, width: 100},
-						{view: "label", label: "Open", css: "right_css", align: "left",width: 200, click: (id, e, trg) => {
-							this.custom_multiselect.show(e.target);
-						}}
-					]});
-			},
-		}, webix.ui.layout, webix.EventSystem, webix.MouseEvents);
-
-		var protoReturn = {
-			view: "CustomMultiselect", label: "Status", labelAlign: "center",
-			options: [
-				{id: 1, check: 0, status: "Open"},
-				{id: 2, check: 0, status: "Available soon"},
-				{id: 3, check: 0, status: "Last deals"},
-				{id: 4, check: 0, status: "Not Available"}
-			], value: "Select"
+webix.protoUI({
+	name: "CustomMultiselect",
+	$init:function(config) {
+		var show_id = config.id + "_custom_select";
+		var statPopup = {
+			view: "popup", css: "custom_popup", id: show_id,
+			body: {
+				rows: [
+					{view: "datatable", header: false, scroll: false, autoheight: true, autowidth: true,
+					columns: [
+						{id: "check", template: "{common.checkbox()}", width: 25, css: "checkbox_section"},
+						{id: "status", width: 225, css: "status_section"}
+					],
+					data: config.options
+				},
+				{view: "button", type: "form", value: config.value}
+			]}							
 		};
-		return protoReturn;
-	}
-}
+		this.custom_multiselect = webix.ui(statPopup);
+		this.$ready.push(this.showLabel);
+	},
+	defaults: {
+		rows: []
+	},
+	showLabel: function() {
+		var me = this;
+		var config = me.config;
+		console.log(config);
+		console.log("Inside label");
+		me.addView({
+			css: "custom_css", cols: [
+				{view: "label", label: config.label, align: config.labelAlign, width: 100},
+				{view: "label", label: "", css: "right_css", align: "left",width: 200, click: (id, e, trg) => {
+					this.custom_multiselect.show(e.target);
+				}}
+			]});
+	},
+}, webix.ui.layout, webix.EventSystem, webix.MouseEvents);
