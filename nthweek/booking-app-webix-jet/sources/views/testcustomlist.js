6531562bd8868c1f1a50946c88ee144cfd2e7b76
sriteja <sritejaayyagari01@gmail.com>
2018-12-14 18:16:48 +0530

    Custom component working. More customisation needed.

--- nthweek/booking-app-webix-jet/sources/views/testcustomlist.js
+++ nthweek/booking-app-webix-jet/sources/views/testcustomlist.js
@@ -5,59 +5,51 @@ export default class TestCustomList extends JetView {
 		webix.protoUI({
 			name: "CustomMultiselect",
 			$init:function(config) {
-				this.$ready.push(this.showDatatable);
+				var show_id = config.id + "_custom_select";
+				var statPopup = {
+					view: "popup", css: "custom_popup", id: show_id,
+					body: {
+						rows: [
+							{view: "datatable", header: false, scroll: false, autoheight: true, autowidth: true,
+							columns: [
+								{id: "check", template: "{common.checkbox()}", width: 25, css: "checkbox_section"},
+								{id: "status", width: 225, css: "status_section"}
+							],
+							data: config.options
+						},
+						{view: "button", type: "form", value: config.value}
+					]}							
+				};
+				this.custom_multiselect = webix.ui(statPopup);
+				this.$ready.push(this.showLabel);
 			},
 			defaults: {
 				rows: []
 			},
-			showDatatable: function() {
+			showLabel: function() {
 				var me = this;
 				var config = me.config;
 				console.log(config);
-				me.addView({ width: 200,
-					cols: [
-						{view: "label", label: config.label, align: config.align, width: 100},
-						{view: "label", label: "Open", align: "left"}
+				console.log("Inside label");
+				me.addView({
+					css: "custom_css", cols: [
+						{view: "label", label: config.label, align: config.labelAlign, width: 100},
+						{view: "label", label: "Open", css: "right_css", align: "left",width: 200, click: (id, e, trg) => {
+							this.custom_multiselect.show(e.target);
+						}}
 					]});
-			}			
-		}, webix.ui.layout);
+			},
+		}, webix.ui.layout, webix.EventSystem, webix.MouseEvents);
 
 		var protoReturn = {
-			view: "CustomMultiselect", label: "Status", align: "right"
+			view: "CustomMultiselect", label: "Status", labelAlign: "center",
+			options: [
+				{id: 1, check: 0, status: "Open"},
+				{id: 2, check: 0, status: "Available soon"},
+				{id: 3, check: 0, status: "Last deals"},
+				{id: 4, check: 0, status: "Not Available"}
+			], value: "Select"
 		};
 		return protoReturn;
 	}
 }
-
-				// view: "popup", width: 100, 
-				// body: {
-				// 	rows: [
-				// 		{view: "datatable", header: false,
-				// 		columns: [
-				// 			{id: "check", template: "{common.checkbox()}", width: 25, css: "checkbox_section"},
-				// 			{id: "status", width: 225, css: "status_section"}
-				// 		],
-				// 		data:  [
-				// 			{id: 1, check: 0, status: "Open"},
-				// 			{id: 2, check: 0, status: "Available soon"},
-				// 			{id: 3, check: 0, status: "Last deals"},
-				// 		]
-				// 	},
-				// 	{view: "button", type: "form", value: "Select"}
-				// ]}
-
-
-				// 		{rows: [
-				// 			{view: "datatable", header: false, scroll: false, autoheight: true, autowidth: true,
-				// 				columns: [
-				// 				{id: "check", template: "{common.checkbox()}", width: 25, css: "checkbox_section"},
-				// 				{id: "status", css: "status_section", width: 150}
-				// 			],
-				// 			data: [
-				// 				{id: 1, check: 0, status: "Open"},
-				// 				{id: 2, check: 0, status: "Available soon"},
-				// 				{id: 3, check: 0, status: "Last deals"},
-				// 			]
-				// 		},
-				// 		{view: "button", type: "form", label: "Select"}]
-				// }
